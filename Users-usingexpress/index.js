dfcf25f0292a8a9b29378a72c9f6c93de5fdc3ae
Saman Gaziani <sg02494@st.habib.edu.pk>
2018-06-06 17:12:35 -0700

    Done

--- Users-usingexpress/index.js
+++ Users-usingexpress/index.js
@@ -17,23 +17,23 @@ app.get('/users/:id', (req,res)=>
     res.send(users.getUserById(req.params.id));
 });
 
-app.post('users/', (req,res) =>
+app.post('/users', (req,res) =>
 {
-    users.addUser(req.body);
+    users.addUser(req.body.user);
     res.send(users.getUsers());
 });
 
-app.put('users/:id',(req,res)=>
+app.put('/users/:id',(req,res)=>
 {
     users.updateUser(req.params.id , req.body.user);
     res.send(users.getUsers());
-})
+});
 
-app.delete('users/:id', (req,res)=>
+app.delete('/users/:id', (req,res)=>
 {
     const user = users.deleteUser(req.params.id);
     res.send(user);
-})
+});
 
 app.listen(3000,()=>
     console.log('Listening to port 3000..')
--- Users-usingexpress/users.js
+++ Users-usingexpress/users.js
@@ -1,11 +1,11 @@
 const users = 
 [
-    {   id: "1",
+    {   id: 1,
         name: "Ahsan Ayaz",
         email: "ahsan.ayaz@iomechs.com"
     }, 
     {
-        id: "2",
+        id: 2,
         name: "Siraj Ul Haq",
         email: "sirajulhaq@koderlabs.com"
     }    
@@ -17,20 +17,21 @@ currentId = 2;
 exports.addUser = function(object)
 {
     currentId++;
-    users.push({id: currentId, name: object.name, email: object.email});
+    users.push({"id": currentId, "name": object.name , "email": object.email});
 };
 
 exports.deleteUser = function(id)
 {
     const user = users.find(user => {
-        return user.id === id;});
-    return users.pop(user);
+        return user.id === parseInt(id);});
+    const index = users.indexOf(user);
+    return users.splice(index,1);
 };
 
 exports.updateUser = function(id ,object)
 {
     const user = users.find(user => {
-        return user.id === id;});
+        return user.id === parseInt(id);});
     user.name = object.name;
     user.email = object.email;
     return user;
@@ -44,6 +45,6 @@ exports.getUsers = function()
 exports.getUserById = function(id)
 {
     const user = users.find(user => {
-        return user.id === id;});
+        return user.id === parseInt(id);});
     return user;
 };
