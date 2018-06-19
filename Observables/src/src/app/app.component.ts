35dc2668b26f9216cd3bc8f3cfc09096f2fcf34c
Saman Gaziani <sg02494@st.habib.edu.pk>
2018-06-19 05:11:39 -0700

    Done

---
+++ Observables/src/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AppService } from './services/app.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 6';
+  box = '';
+  circle = '';
+  constructor(private appService: AppService) {
+  }
+
+  
+  changeBoxColor()
+  {
+    this.appService.$boxColorSubject.next(this.box);
+  }
+
+  changeCircleColor() {
+    this.appService.$circleColorSubject.next(this.circle);
+  }
+}
